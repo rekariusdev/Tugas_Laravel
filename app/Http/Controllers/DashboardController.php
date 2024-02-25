4f16bd1ad8d5ea4fef9ca9e810d2f0c45d39728e
rekariusdev <c2242102026@gmail.com>
2024-02-25 18:31:51 +0700

    File Tugas Rpl

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+ public function index()
+ {
+     return view('backend.content.dashboard');
+
+ }
+ public function profile(){
+return view('backend.content.profile');
+ }
+ }
